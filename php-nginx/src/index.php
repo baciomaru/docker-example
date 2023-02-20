baa57c042d2c2c2d5048518a93a46770d4144b83
baciomaru <mss.maruyama@gmail.com>
2023-02-20 11:34:50 +0900

    add php

---
+++ php-nginx/src/index.php
@@ -0,0 +1,11 @@
+<?php
+$filename = 'counter.dat';
+
+$fp = fopen($filename, "r+");
+$count = fgets($fp,32);
+$count++;
+fseek($fp, 0);
+fputs($fp, $count);
+flock($fp, LOCK_UN);
+fclose($fp);
+echo $count;
